542fcc4375a03b92e72a1cba003008c282597b78
bukanspot <spot@tracert.id>
2022-12-18 17:01:02 +0800

    layout form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
 
-  <h1>Hello, world!</h1>
+<div class="container col-md-3 mt-5">
+  <div class="mb-3">
+    <label for="nomor-induk-siswa" class="form-label">Nomor Induk Siswa</label>
+    <input type="text" class="form-control" id="nomor-induk-siswa" placeholder="Nomor Induk Siswa">
+  </div>
+  <div class="mb-3">
+    <label for="nama-siswa" class="form-label">Nama Siswa</label>
+    <input type="text" class="form-control" id="nama-siswa" placeholder="Nama Siswa">
+  </div>
+</div>
+
+
+{{-- <h1>Hello, world!</h1> --}}
+
 
 @endsection
