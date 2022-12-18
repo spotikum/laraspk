2f2318535e7a7f18074692010db126591ab1f0f6
MangnuJR <mangjr168@gmail.com>
2022-12-18 18:16:43 +0800

    update form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,6 +11,61 @@
     <label for="nama-siswa" class="form-label">Nama Siswa</label>
     <input type="text" class="form-control" id="nama-siswa" placeholder="Nama Siswa">
   </div>
+
+  <form class="row g-3">
+    <div class="col-md-6">
+      <label for="fisika" class="form-label">Fisika</label>
+      <input type="fisika" class="form-control" id="fisika">
+    </div>
+    <div class="col-md-6">
+      <label for="kimia" class="form-label">Kimia</label>
+      <input type="kimia" class="form-control" id="kimia">
+    </div>
+    <div class="col-md-6">
+      <label for="matematika" class="form-label">Matematika</label>
+      <input type="matematika" class="form-control" id="matematika">
+    </div>
+    <div class="col-md-6">
+      <label for="biologi" class="form-label">Biologi</label>
+      <input type="biologi" class="form-control" id="biologi">
+    </div>
+  </form>
+
+  <form class="row g-3">
+    <div class="col-md-6">
+      <label for="geografi" class="form-label">Geografi</label>
+      <input type="geografi" class="form-control" id="geografi">
+    </div>
+    <div class="col-md-6">
+      <label for="sosiologi" class="form-label">Sosiologi</label>
+      <input type="sosiologi" class="form-control" id="sosiologi">
+    </div>
+    <div class="col-md-6">
+      <label for="antropologi" class="form-label">Antropologi</label>
+      <input type="antropologi" class="form-control" id="antropologi">
+    </div>
+    <div class="col-md-6">
+      <label for="ekonomi" class="form-label">Ekonomi</label>
+      <input type="ekonomi" class="form-control" id="ekonomi">
+    </div>
+  </form>
+  
+    <div class="mb-3">
+      <label for="alamat" class="form-label">Pilih Jurusuan</label>
+      <div class="form-check">
+          <input class="form-check-input" type="radio" value="1" name="jurusan[]" id="flexCheckDefault1">
+          <label class="form-check-label" for="flexCheckDefault1">
+              IPA
+          </label>
+      </div>
+      <div class="form-check">
+          <input class="form-check-input" type="radio" value="2" name="jurusan[]" id="flexCheckDefault2">
+          <label class="form-check-label" for="flexCheckDefault2">
+              IPS
+          </label>
+      </div>
+  </div>
+  </div>
 </div>
 
 
